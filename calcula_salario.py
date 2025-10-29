f8ccaa9b0698e42d11ff5b9008b59a42cb955875
Kaio Eduardo dos Santos <keduardokaio29@gmail.com>
2025-10-29 15:36:07 -0300

    Add files via upload

---
+++ calcula_salario.py
@@ -0,0 +1,5 @@
+horas_trabalhadas = int(input("Quantas horas por mês você trabalhou?"))
+salario_por_hora = float(input("Quantos você recebe por hora?"))
+
+total = horas_trabalhadas * salario_por_hora
+print(f"Você trabalhou por {horas_trabalhadas} horas no mês, e ganha R${salario_por_hora} por hora, e seu salario no final do mês é:{total}")
